d0f292dca601b188d26678d84c74fb0ed9e6889b
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-06 11:23:55 -0300

    enum endereco de ativfixacacao

--- ativFixacao/ativFixacao/src/main/java/com/example/ativfixacao/model/Funcionario.java
+++ ativFixacao/ativFixacao/src/main/java/com/example/ativfixacao/model/Funcionario.java
@@ -1,10 +1,12 @@
 package com.example.ativfixacao.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -34,4 +36,7 @@ public class Funcionario {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Endereco endereco;
 }
