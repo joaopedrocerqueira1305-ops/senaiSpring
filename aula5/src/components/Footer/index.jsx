a9d4ab83d2d2595cb7170a4a85b826327b1281c2
joaopedrocerqueira1305-ops <joaopedrocerqueira1305@gmail.com>
2025-11-18 11:25:31 -0300

    Delete aula5 directory

--- aula5/src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import './style.css';
-
-export default function Footer(){
-    return(
-        <footer className='footer'>
-            <p>&copy; {anoAtual} - Todos os direitos reservados.</p>
-        </footer>
-    );
-}
