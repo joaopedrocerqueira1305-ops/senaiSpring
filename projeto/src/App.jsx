888c19e381d7da30cfce562af1fb74cc1fb9c1d6
joaopedrocerqueira1305-ops <joaopedrocerqueira1305@gmail.com>
2025-12-01 11:24:54 -0300

    Add files via upload

---
+++ projeto/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import AdicionarUsuario from './components/AdicionarUsuario'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+        <h1>Senai</h1>
+        <AdicionarUsuario />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ projeto/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+// src\components\Header\index.jsx
+
+import './style.css';
+
+function Header() {
+    return (
+        <header className='header-container'>
+            <div className='logo'>Sistema de Clientes</div>
+        </header>
+    );
+}
+
+export default Header;
